e39d8f8810352842fd046722764436647ee9c3be
Nigel Horne <njh@bandsman.co.uk>
2025-10-14 20:00:37 -0400

    Release 0.04

--- lib/HTML/Genealogy/Map.pm
+++ lib/HTML/Genealogy/Map.pm
@@ -21,11 +21,11 @@ HTML::Genealogy::Map - Extract and map genealogical events from a GEDCOM file
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 DESCRIPTION
 
